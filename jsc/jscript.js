acfb5b1a07a0aedabd3412bbff4c58dcc701a786
haviv-haviv <82034415+haviv-haviv@users.noreply.github.com>
2021-06-06 21:48:00 +0300

    Add files via upload

---
+++ jsc/jscript.js
@@ -0,0 +1,72 @@
+
+//let myFnumber = 23;
+//let mySnumber = 66;
+
+//if (myFnumber < mySnumber){
+//   document.write('the beggest number is'+ mySnumber);
+//
+
+//--------------------------
+
+//let m = [-2,3,6];
+//let s ='+';
+
+//if (m[0] + m[1] + m[2] ){
+//   document.write(s);
+//}
+
+//----------------------------
+
+//let myarr = [4,9,6];
+
+//if(myarr[0] < myarr[1] && myarr[0] < myarr[2] && myarr[1] > myarr[2] ){
+//    console.log([myarr[0] , myarr[2] , myarr[1]]);   
+//}else{
+//    document.write('non');
+//}
+
+//-----------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
